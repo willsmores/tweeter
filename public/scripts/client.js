1d574f709dc59845c81f833944764b90b05061e7
Steve Willsmore <scwillsmore@gmail.com>
2022-12-07 14:48:07 -0500

    Add functionality for rendering tweets dynamically. Tweaked css.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+
+$(document).ready(function() {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = (tweetArr) => {
+
+    tweetArr.forEach(tweet => {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    })
+  };
+
+
+  const createTweetElement = (tweetData) => {
+    // console.log(tweetData);
+    let $tweet = `
+    <article>
+      <header>
+        <div>
+          <img src="${tweetData.user.avatars}">
+          <h3>${tweetData.user.name}</h3>
+        </div>
+        <h4>${tweetData.user.handle}</h4>
+      </header>
+      <p class="tweet-text">${tweetData.content.text}</p>
+      <footer>
+        <p>${tweetData.created_at}</p>
+        <div class="tweet-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    `;
+
+    return $tweet;
+  };
+
+  // const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page
+
+  renderTweets(data);
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
 
-  $('#tweet-text').on('input', function() {
-    const charCount = this.value.length;
-    // console.log(charsRemaining - charCount);
-    let charsRemaining = 140 - charCount;
-    
-    $('.counter').text(charsRemaining);
+  // Set the total number of characters available for a tweet
+  // Thought it best not to hardcode this value
+  const totalChars = $('.counter').text();
 
+  $('#tweet-text').on('input', function() {
+    
+    // Total length of characters in input
+    const charCount = $(this).val().length;
+
+    // Traverse to sibling element
+    const tweetFooter = $(this).next();
+    // In sibling, find output element
+    const counter = tweetFooter.find('output');
+
+    // Sets the new count
+    const charsRemaining = totalChars - charCount;
+    $(counter).text(charsRemaining);
+
+    // Sets the 'negative count' text colour
     if (charsRemaining < 0) {
-      $('.counter').addClass('counter-negative');
+      $(counter).addClass('counter-negative');
     } else {
-      $('.counter').removeClass('counter-negative');
+      $(counter).removeClass('counter-negative');
     }
   })
 });
