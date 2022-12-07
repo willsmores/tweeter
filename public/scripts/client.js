8913232f86bfd015f81952d4230e290c9fbf64f0
Steve Willsmore <scwillsmore@gmail.com>
2022-12-07 16:46:38 -0500

    Add tweet get request and timeago.js to format timestamp.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,31 +16,21 @@ $(document).ready(function() {
   });
 
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  const loadTweets = () => {
+
+    // $.ajax('/tweets', { method: 'GET' })
+    // .then(function (moreTweets) {
+    //   renderTweets(moreTweets);
+    // });
+
+    // jQuery version
+    $.get("/tweets", function(moreTweets, status) {
+      renderTweets(moreTweets);
+    });
+  }
+
+  loadTweets();
+
 
   const renderTweets = (tweetArr) => {
 
@@ -64,7 +54,7 @@ $(document).ready(function() {
       </header>
       <p class="tweet-text">${tweetData.content.text}</p>
       <footer>
-        <p>${tweetData.created_at}</p>
+        <p class="timeago">${timeago.format(tweetData.created_at)}</p>
         <div class="tweet-icons">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -76,12 +66,4 @@ $(document).ready(function() {
 
     return $tweet;
   };
-
-  // const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-  // $('#tweets-container').append($tweet); // to add it to the page
-
-  renderTweets(data);
 });
